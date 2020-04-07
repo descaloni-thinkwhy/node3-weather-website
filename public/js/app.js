e74c8046ed775f97fe002731b82e816988ea1478
Descaloni_thinkwhy <descaloni@thinkwhy.com>
2020-04-06 21:01:14 -0500

    Seyup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 console.log('Client side javascript file is loaded!')
 
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -14,10 +12,9 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     
     messageOne.textContent = 'Loading...'
-    
     messageTwo.textContent = ''
     
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error)
